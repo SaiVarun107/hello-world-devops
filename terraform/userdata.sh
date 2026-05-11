0483692b2e493f3b4e6e1c92945e5c251b5abb48
SaiVarun107 <saivarunbitra107@gmail.com>
2026-05-11 08:56:09 +0530

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -2,16 +2,36 @@
 
 sudo apt update -y
 
-# Docker Installation
+#################################################
+# INSTALL DOCKER
+#################################################
+
 sudo apt install docker.io -y
+
 sudo systemctl enable docker
+
 sudo systemctl start docker
+
 sudo usermod -aG docker ubuntu
 
-# K3s Installation
-curl -sfL https://get.k3s.io | sh -
+#################################################
+# INSTALL K3S
+#################################################
 
-# Kubectl Access
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
+
+#################################################
+# KUBECONFIG FOR UBUNTU USER
+#################################################
+
 mkdir -p /home/ubuntu/.kube
+
 sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config
+
 sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
+
+#################################################
+# ENV VARIABLE
+#################################################
+
+echo 'export KUBECONFIG=/home/ubuntu/.kube/config' >> /home/ubuntu/.bashrc
